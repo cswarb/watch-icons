177fcd9b260520f50d37e9b85aa8665610e3f476
Chris Swarbrick <chris.swarb@gmail.com>
2022-04-06 22:41:59 +0100

    Added watch face tests

--- src/watch/animations/animations.tsx
+++ src/watch/animations/animations.tsx
@@ -1,18 +1,31 @@
 import { Map } from 'immutable';
+import { useEffect, useState } from 'react';
 import { FallbackAnimation } from './Fallback';
 import { JumboAnimation } from './royal-oak/Jumbo';
 import { LangeAnimation } from './zeitwerk/Lange';
 
 export const WATCH_ANIMATIONS = Map <string, any>()
     .set('Zeitwerk', LangeAnimation)
-    .set('Royal Oak 15202', JumboAnimation);
+    .set('Royal Oak', JumboAnimation);
 
 export const WatchAnimationFactory = ({ model, ...props }: { model: string }) => {
-    console.log('called');
-    
     const WatchAnimation = WATCH_ANIMATIONS.get(model) || FallbackAnimation;
+    const [time, setTime] = useState(new Date());
+
+    function updateTime() {
+        setTime(new Date());
+    };
+
+    useEffect(() => {
+        // setInterval(() => {
+            // setTime(new Date());
+        // }, 1000);
+    }, []);
 
     return (
-        <WatchAnimation {...props} />
+        <>
+            <button type="button" onClick={() => updateTime()}>update time</button>
+            <WatchAnimation time={time} {...props} />
+        </>
     )
 }
--- src/watch/animations/royal-oak/Jumbo.tsx
+++ src/watch/animations/royal-oak/Jumbo.tsx
@@ -1,9 +1,146 @@
 import * as d3 from 'd3';
 import { useEffect, useRef, useState } from 'react';
 
-export const JumboAnimation = (props: any) => {
-    console.log('JumboAnimation');
+export const JumboAnimation = ({ time, ...props }: any) => {
+    console.log('JumboAnimation', time);
     const ref = useRef<any>();
+    const dimensions: any = {
+        width: 500,
+        height: 500,
+        margin: {
+            top: 50,
+            right: 0,
+            bottom: 50,
+            left: 0
+        }
+    };
+
+    dimensions.boundedWidth = dimensions.width - dimensions.margin.left - dimensions.margin.left;
+    dimensions.boundedheight = dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
+
+    const smh = (time: Date): { day: number, date: number, month: number, year: number, seconds: number, minutes: number, hours: number, millis: number } => {
+        return {
+            day: time.getDay(),
+            date: time.getDate(),
+            month: time.getMonth(),
+            year: time.getFullYear(),
+            seconds: time.getSeconds(),
+            minutes: time.getMinutes(),
+            hours: time.getHours(),
+            millis: time.getMilliseconds()
+        }
+    };
+
+    const initChart = () => {
+        const svg = d3.select(ref.current);
+        svg.attr('width', dimensions.width).attr('height', dimensions.height);
+        const stage = svg.append('g').attr('class', 'stage').style('transform', `translate(${dimensions.margin.left}px, ${dimensions.margin.top}px)`);;
+    };
+
+    const updateChart = (time: any) => {
+        const st: any = d3.select(ref.current);
+        const stage = st.select('.stage');
+
+        stage.selectAll('.time').data([time]).join((enter: any) => {
+            return enter.append('text').attr('class', 'time').attr('x', 0).attr('y', 0).text((d: any) => d);
+        }, (update: any) => {
+            return update.text((d: any) => d);
+        }, (exit: any) => {
+            return exit.remove();
+        });
+
+        stage.selectAll('.seconds').data([time]).join((enter: any) => {
+            const cir = enter.append('circle')
+                .attr('class', 'seconds')
+                .attr('cx', 0)
+                .attr('cy', 0)
+                .attr('r', 25)
+                .style('transform-box', 'fill-box')
+                .style('transform-origin', 'center')
+                .style('transform', (d: any) => {
+                    const s = smh(d).seconds;
+                    const degs = 360 / 60;
+                    return `rotate(${degs*s}deg)`;
+                });
+
+            return enter;
+        }, (update: any) => {
+            return update
+                .style('transform', (d: any) => {
+                    const s = smh(d).seconds;
+                    const degs = 360 / 60;
+                    return `rotate(${degs * s}deg)`;
+                });
+        }, (exit: any) => {
+            return exit.remove();
+        });
+
+        stage.selectAll('.minutes').data([time]).join((enter: any) => {
+            const cir = enter.append('circle')
+                .attr('class', 'minutes')
+                .attr('cx', 50)
+                .attr('cy', 100)
+                .attr('fill', 'red')
+                .attr('r', 50)
+                .style('transform-box', 'fill-box')
+                .style('transform-origin', 'center')
+                .style('transform', (d: any) => {
+                    const m = smh(d).minutes;
+                    const degs = 360 / 60;
+                    return `rotate(${degs * m}deg)`;
+                });
+
+            return enter;
+        }, (update: any) => {
+            return update
+                .style('transform', (d: any) => {
+                    const m = smh(d).minutes;
+                    const degs = 360 / 60;
+                    return `rotate(${degs * m}deg)`;
+                });
+        }, (exit: any) => {
+            return exit.remove();
+        });
+
+        stage.selectAll('.hours').data([time]).join((enter: any) => {
+            const cir = enter.append('circle')
+                .attr('class', 'hours')
+                .attr('cx', 50)
+                .attr('cy', 200)
+                .attr('fill', 'green')
+                .attr('r', 50)
+                .style('transform-box', 'fill-box')
+                .style('transform-origin', 'center')
+                .style('transform', (d: any) => {
+                    const h = smh(d).hours;
+                    const degs = 360 / 12;
+                    const twentyFourHourDegrees = (degs * h) % 360;
+                    return `rotate(${twentyFourHourDegrees}deg)`;
+                });
+
+            return enter;
+        }, (update: any) => {
+            return update
+                .style('transform', (d: any) => {
+                    const h = smh(d).hours;
+                    const degs = 360 / 12;
+                    const twentyFourHourDegrees = (degs * h) % 360;
+                    return `rotate(${twentyFourHourDegrees}deg)`;
+                });
+        }, (exit: any) => {
+            return exit.remove();
+        });
+    };
+
+    useEffect(() => {
+        console.log('initChart');
+        initChart();
+    }, []);
+
+    useEffect(() => {
+        console.log('updateChart');
+        updateChart(time);
+    }, [time]);
 
     return (
         <>
